06c38f79679b44a06df87b8d1fc2d716de80ed25
DaviWT <43670692+DaviWT@users.noreply.github.com>
2018-10-10 09:19:56 +0100

    Update writenoncanonical.c

--- proj1/writenoncanonical.c
+++ proj1/writenoncanonical.c
@@ -87,7 +87,7 @@ void llopen(int fd)
   newtio.c_lflag = 0;
 
   newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
-  newtio.c_cc[VMIN]     = 5;   /* blocking read until 1 char received */
+  newtio.c_cc[VMIN]     = 0;   /* blocking read until 1 char received */
 /* 
   VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a 
   leitura do(s) pr�ximo(s) caracter(es)
