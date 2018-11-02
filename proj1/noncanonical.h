4269bff5b4540e67a613857c39f1e643e2985a8e
Tomas FIgueiredo <tomasviana_cof@hotmail.com>
2018-11-02 03:59:27 +0000

    Added a few macros and the prototype of new functions

--- proj1/noncanonical.h
+++ proj1/noncanonical.h
@@ -7,12 +7,31 @@
 #include <string.h>
 #include <unistd.h>
 
+#define FLAG 				0x7E
+#define A 					0x03
+#define SET_C 				0x03
+#define UA_C				0x07
+#define SET_BCC1 			A^SET_C
+#define UA_BCC1 			A^UA_C
+#define C0                  0x00
+#define C1                  0x40
+#define RR_C0               0x05
+#define RR_C1               0x85 
+#define REJ_C0              0x01
+#define REJ_C1              0x81 
+#define DISC_C              0x0B
+
+
 enum state_machine {START, FLAG_RCV, A_RCV, C_RCV, BCC1_RCV, DONE};
 
 void llopen(int fd);
 
-int caughtSET(int fd);
+unsigned char *llread(int fd);
 
-void sendUA(int fd);
+int caughtSUFrame(int fd, unsigned char CFlag);
+
+int hasBCC2();
+
+void sendAcknowlegment(int fd, unsigned char c);
 
 void llclose(int fd);
