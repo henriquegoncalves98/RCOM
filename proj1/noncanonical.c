66aedfe9a1103927fe22e222d607d20684f82f74
up201608320 <henriqinho20@gmail.com>
2018-11-08 17:30:38 +0000

    receiver done

--- proj1/noncanonical.c
+++ proj1/noncanonical.c
@@ -11,7 +11,7 @@ volatile int STOP=FALSE;
 
 int messageToReceive = 0;
 struct termios oldtio,newtio;
-int bytesForEachPacket = 100;
+int bytesForEachPacket;
 int BAUDRATE;
 
 
@@ -19,7 +19,7 @@ int main(int argc, char** argv) {
 	int fd;
 	
 	//PORT CONFIGURATION AND OPENING PORTS
-	if(argc < 3) {
+	if(argc < 4) {
 		printf("Invalid number of arguments! \n");
 		exit(1);
 	  }
@@ -35,7 +35,9 @@ int main(int argc, char** argv) {
 		perror(argv[1]); exit(-1);
 	}
 	//END OF PORT CONFIGURATION AND OPENING PORTS
-	BAUDRATE = getBaudrate(atoi(argv[2]));
+
+	bytesForEachPacket = atoi(argv[2]);
+	BAUDRATE = getBaudrate(atoi(argv[3]));
 	llopen(fd);
 
 	unsigned char *startFrame = (unsigned char *)malloc(0);
@@ -408,8 +410,6 @@ void getPacketInfo(unsigned char *fileData, int *fileDataSize, unsigned char *pa
 
 	(*fileDataSize) += (packetSize - 4);
 
-	for(i=0; i<(*fileDataSize); i++) 
-		printf("%x ", fileData[i]);
 
 	free(fileData2);
 }
@@ -443,19 +443,40 @@ void llclose(int fd) {
 
 int getBaudrate(int baudrate) {
 	switch (baudrate) {
-
-		case 2400:
-			return B2400;
-		case 4800:
-			return B4800;
-		case 9600:
-			return B9600;
-		case 19200:
-			return B19200;
-		case 38400:
-			return B38400;
-
-		default:
-			return B38400;
+	case 0:
+		return B0;
+	case 50:
+		return B50;
+	case 75:
+		return B75;
+	case 110:
+		return B110;
+	case 134:
+		return B134;
+	case 150:
+		return B150;
+	case 200:
+		return B200;
+	case 300:
+		return B300;
+	case 600:
+		return B600;
+	case 1200:
+		return B1200;
+	case 1800:
+		return B1800;
+	case 2400:
+		return B2400;
+	case 4800:
+		return B4800;
+	case 9600:
+		return B9600;
+	case 19200:
+		return B19200;
+	case 38400:
+		return B38400;
+	
+	default:
+		return B38400;
 	}
 }
